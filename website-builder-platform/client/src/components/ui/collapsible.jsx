37191f6adc8cead8be26411e9abbe0b32a1a6323
sakshi_er <sakshi.anand@lmsathena.com>
2025-12-26 15:11:38 +0530

    ui

---
+++ website-builder-platform/client/src/components/ui/collapsible.jsx
@@ -0,0 +1,8 @@
+import * as CollapsiblePrimitive from "@radix-ui/react-collapsible";
+import { Collapsible, CollapsibleTrigger, CollapsibleContent } from './ui/collapsible';
+
+const Collapsible = CollapsiblePrimitive.Root;
+const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger;
+const CollapsibleContent = CollapsiblePrimitive.CollapsibleContent;
+
+export { Collapsible, CollapsibleTrigger, CollapsibleContent };
