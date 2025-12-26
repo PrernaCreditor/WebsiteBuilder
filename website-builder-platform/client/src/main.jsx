b4a4fca8ea90f5a169bd56bfce4c039feffb2da3
amaan-sys <amaan@creditoracademy.com>
2025-12-26 15:16:55 +0530

    latesttt

--- website-builder-platform/client/src/main.jsx
+++ website-builder-platform/client/src/main.jsx
@@ -1,8 +1,8 @@
 // src/main.jsx
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./styles/globals.css"; // 👈 use our global CSS
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
